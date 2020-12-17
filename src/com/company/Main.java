031ad1e646e55b07b113fc69e93d98d28cac8d57
jolyjolybee <dpqmszlxl01@gmail.com>
2020-12-17 14:12:42 +0900

    add hello alchera

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	System.out.println("Hello world!");
 	System.out.println("Hello world!");
 	System.out.println("Hello world!");
+	System.out.println("Hello ALCHERA");
 
     }
 }
