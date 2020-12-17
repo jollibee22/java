9a747231abb21874d2d4266f1dbe1a6f4651bb28
jolyjolybee <dpqmszlxl01@gmail.com>
2020-12-17 16:40:08 +0900

    사칙연산

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
     	int num1, num2;
-    	double result;
     	Scanner sc = new Scanner(System.in);
     	num1 = sc.nextInt();
     	num2 = sc.nextInt();
-    	result = (double)num1/num2;
-    	System.out.println(result);
+    	System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1&num2);
 
 	}
 }
