7091e732f1160c609efda9cc9c89fbc3da24f4e7
jolyjolybee <dpqmszlxl01@gmail.com>
2020-12-17 17:05:07 +0900

    [백준]10430

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    	int num1, num2;
     	Scanner sc = new Scanner(System.in);
-    	num1 = sc.nextInt();
-    	num2 = sc.nextInt();
-    	System.out.println(num1+num2);
-		System.out.println(num1-num2);
-		System.out.println(num1*num2);
-		System.out.println(num1/num2);
-		System.out.println(num1%num2);
+    	int A = sc.nextInt();
+    	int B = sc.nextInt();
+		int C = sc.nextInt();
+
+    	System.out.println((A+B)%C);
+		System.out.println(((A%C)+(B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
 
 	}
 }
