04406539a98f26b6c9de8a07b19fd8fbcb2fd538
jolyjolybee <dpqmszlxl01@gmail.com>
2020-12-17 18:12:04 +0900

    [백준]9498

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,17 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
-    	int num2 = sc.nextInt();
-    	if(num1>num2)
-    		System.out.println(">");
-    	else if(num1<num2)
-    		System.out.println("<");
+    	//int num2 = sc.nextInt();
+    	if ((num1>=90)&&(num1<=100))
+    		System.out.println("A");
+    	else if ((num1>=80)&&(num1<=89))
+    		System.out.println("B");
+    	else if ((num1>=70)&&(num1<=79))
+    		System.out.println("C");
+    	else if ((num1>=60)&&(num1<=69))
+    		System.out.println("D");
     	else
-    		System.out.println("==");
+    		System.out.println("F");
 	}
 
 }
