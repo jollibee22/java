d5b8acd828ee93bd299593d7f10e40b40a3d8475
jollibee22 <dpqmszlxl01@gmail.com>
2020-12-18 20:55:21 +0900

    [백준]2753 윤년

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,16 +8,10 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
     	//int num2 = sc.nextInt();
-    	if ((num1>=90)&&(num1<=100))
-    		System.out.println("A");
-    	else if ((num1>=80)&&(num1<=89))
-    		System.out.println("B");
-    	else if ((num1>=70)&&(num1<=79))
-    		System.out.println("C");
-    	else if ((num1>=60)&&(num1<=69))
-    		System.out.println("D");
+    	if (((num1%4==0)&&(num1%100!=0))||(num1%400==0))
+    		System.out.println("1");
     	else
-    		System.out.println("F");
+    		System.out.println("0");
 	}
 
 }
