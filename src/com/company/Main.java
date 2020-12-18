f2bb13a1152bb7f3eb58608328614b0055facb97
jollibee22 <dpqmszlxl01@gmail.com>
2020-12-18 21:30:36 +0900

    [백준]2884 시계

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,29 @@ public class Main {
     public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-    	//int num2 = sc.nextInt();
-    	if (((num1%4==0)&&(num1%100!=0))||(num1%400==0))
-    		System.out.println("1");
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int rm= m-45;
+
+    	if (rm<0)
+		{
+			if(h!=0)
+			{
+				h--;
+				rm = 60 + rm;
+			}
+			else
+			{
+				h=23;
+				rm=60+rm;
+			}
+			System.out.println(h+" "+rm);
+
+		}
+
     	else
-    		System.out.println("0");
+    		System.out.println(h+" "+rm);
+
 	}
 
 }
